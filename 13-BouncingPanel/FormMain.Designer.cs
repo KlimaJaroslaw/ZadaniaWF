98448127c6fb36cd7e8891841e4f0e76c045b6f4
JaroslawKlima <jaroslaw.klima@aprosoft.pl>
2021-07-30 14:58:51 +0200

    13-BouncingPanel

---
+++ 13-BouncingPanel/FormMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace _13_BouncingPanel
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(314, 176);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(90, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 150;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(177, 124);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(90, 90);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(472, 104);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(90, 90);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(373, 312);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(90, 90);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(215, 289);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(90, 90);
+            this.panel5.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormMain";
+            this.Text = "Bouncing panel";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ 13-BouncingPanel/FormMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_BouncingPanel
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        private int RandomNumber(int a,int b)
+        {
+            return random.Next(a, b);
+        }
+
+        int maxX;
+        int maxY;
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            FormMain form = sender as FormMain;
+            maxX = form.Size.Width;
+            maxY = form.Size.Height;
+
+            Start();
+        }
+        
+        
+        
+        private void Start()
+        {
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Panel panel = new Panel();
+            for (int i = 0; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        panel = panel1;
+                        break;
+                    case 1:
+                        panel = panel2;
+                        break;
+                    case 2:
+                        panel = panel3;
+                        break;
+                    case 3:
+                        panel = panel4;
+                        break;
+                    case 4:
+                        panel = panel5;
+                        break;
+                    default:
+                        break;
+                }
+                int x;
+                int y;
+                int xPanel;
+                int yPanel;
+
+                xPanel = panel.Location.X;
+                yPanel = panel.Location.Y;
+
+                do
+                {
+                    x = RandomNumber(-75, 75);
+                    y = RandomNumber(-75, 75);
+
+                } while (((xPanel + x) > maxX - panel.Width) || ((xPanel + x) < 0) || ((yPanel + y) > maxY - panel.Height) || ((yPanel + y) < 0));
+
+                xPanel += x;
+                yPanel += y;
+
+                panel.Location = new Point(xPanel, yPanel);
+                panel.BackColor = Color.FromArgb(RandomNumber(0, 255), RandomNumber(0, 255), RandomNumber(0, 255));
+            }
+            
+        }
+    }
+}
